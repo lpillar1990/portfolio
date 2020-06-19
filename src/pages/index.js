a744c42f5c075be47ba519ec2afd426e4180245a
lpillar1990 <lpillar1990@googlemail.com>
2020-06-19 16:12:26 +0100

    form02

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,14 +61,7 @@ const HomeIndex = () => {
           </p>
           <div className="row">
             <div className="8u 12u$(small)">
-              <form
-                name="contact"
-                method="POST"
-                class="form"
-                netlify-honeypot="bot-field"
-                data-netlify="true"
-                netlify
-              >
+              <form name="contact" action="POST" data-netlify="true">
                 <div className="row uniform 50%">
                   <div className="6u 12u$(xsmall)">
                     <input
